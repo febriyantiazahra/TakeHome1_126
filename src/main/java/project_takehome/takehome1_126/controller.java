73a36de87883d747b83db75a7f0ff758d120f800
febriyantiazahra <febriyantiazhraa@gmail.com>
2022-10-30 18:55:03 +0700

    mengedit file view dan controller

--- src/main/java/project_takehome/takehome1_126/controller.java
+++ src/main/java/project_takehome/takehome1_126/controller.java
@@ -5,6 +5,7 @@
  */
 package project_takehome.takehome1_126;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestParam;
  *
  * @author Febriyanti Azahra
  */
+@Controller
 public class controller {
           
     jualbeli JB = new jualbeli();
