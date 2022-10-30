c81763ca4dee51075c9fd9b4244c8e86ad8a337b
febriyantiazahra <febriyantiazhraa@gmail.com>
2022-10-30 18:25:11 +0700

    membuat project spring boot

---
+++ src/main/java/project_takehome/takehome1_126/Takehome1126Application.java
@@ -0,0 +1,13 @@
+package project_takehome.takehome1_126;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Takehome1126Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Takehome1126Application.class, args);
+	}
+
+}
---
+++ src/test/java/project_takehome/takehome1_126/Takehome1126ApplicationTests.java
@@ -0,0 +1,13 @@
+package project_takehome.takehome1_126;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Takehome1126ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
